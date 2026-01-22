b8845614fc65049bbe909c94a1de1f14fb1c7ff7
wodud17711 <wodud17711@gmail.com>
2026-01-22 16:48:56 +0900

    리액트 두번째 수업 커밋

--- src/App.js
+++ src/App.js
@@ -2,15 +2,78 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button, Nav, Navbar, Container} from 'react-bootstrap';
-import { useState } from 'react';
+import { createContext, useState } from 'react';
 import data from './data.js';
 import {Routes, Route, Link, useNavigate, Outlet, useParams} from 'react-router-dom';
 import Detail from './routes/Detail.js';
+import axios from 'axios';
+import Cart from './routes/cart.js';
+
+/*
+
+lifeCycle
+
+페이지에 장착되는 순간 (mount)
+페이지에서 업데이트 되는 순간 (update)
+페이지에서 제거가 되는 순간 (unmount)
+
+
+useEffect
+
+1. 재렌더링 마다(mount or update) 코드를 실행하고 싶으면
+
+  useEffect(()=>{})
+
+2. mount시 1회만 코드를 실행하고 싶으면
+
+  useEffect(()={},[])
+
+3. 특정 state 변경시 useEffect를 실행하려면
+
+  useEffect(()=>{},[count])
+
+4. useEffect 실행 전에 코드를 실행하려면
+
+  uesEffect(()=>{
+
+    return()=.{
+    }
+    
+    })
+
+
+서버와 통신하는 방법
+
+1. GET : 데이터를 서버에서 가지오 올 때
+
+2. POST : 데이터를 서버에 보낼 때
+
+
+서버에 요청하는 방법
+
+1. XMLHttpRequest
+2. fetch()
+3. axios
+
+
+스테이트 전체 공유
+
+1. Context API (리액트 기본문법)
+2. Redux (라이브러리)
+
+*/
+
+
+
+
 
 
 function App() {
 
-  let [shoes] = useState(data);
+  let [shoes, setShoes] = useState(data);
+  let [clickCount, setClickCount] = useState(0);
+  let [재고] = useState([10,11,12]);
+
   let navigate = useNavigate();
 
   return (
@@ -36,15 +99,49 @@ function App() {
                 {
                   shoes.map((a, i)=>{
                     return(
-                    <Card shoes={a} i={i}></Card>
+                    <Card shoes={shoes[i]} i={i}></Card>
                     )
                   })
                 }
               </div>
             </div>
+            <button onClick={()=>{
+
+              if(clickCount === 0){
+                axios.get(`https://nujabescode.github.io/data2.json`)
+                .then((data)=>{
+                  console.log(data.data)
+                  // setShoes(copy => [...copy, ...data.data]);
+                  let copy = [...shoes, ...data.data];
+                  setShoes(copy);
+                  // copy = shoes.concat(data.data);
+                  // setShoes(copy);
+                  setClickCount(1);
+                })
+                .catch(()=>{
+                  console.log('실패')
+                })
+              }else if(clickCount === 1){
+                axios.get(`https://nujabescode.github.io/data3.json`)
+                .then((data)=>{
+                  console.log(data.data)
+                  let copy = [...shoes, ...data.data];
+                  setShoes(copy);
+                  setClickCount(2);
+                })
+                .catch(()=>{
+                  console.log('실패')
+                })
+              }else{
+                alert("더 이상 불러올 데이터가 없습니다.")
+              }
+            }}>더보기</button>
          </>
         }/>
-        <Route path='/detail/:id' element={<Detail shoes={shoes}/>}/>
+        <Route path='/detail/:id' element={
+            <Detail shoes={shoes}/>
+        }/>
+        <Route path='/cart' element={<Cart/>}/>
 
         <Route path='/about' element={<About/>}>
           <Route path='member' element={<div>멤버페이지</div>}/>
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,13 +1,38 @@
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Nav from 'react-bootstrap/Nav';
+
 
 function Detail(props){
 
+
+
+
+    let [count, setCount] = useState(0)
+    let [alert, setAlert] = useState(true)
+    let [탭, 탭변경] = useState(0)
+
+    useEffect(()=>{
+        setTimeout(()=>{setAlert(false)},2000);
+        
+    },[])
+
+
     let {id} = useParams();
-    console.log(id);
+    console.log(count);
 
     return(
 
         <div className='container'>
+            {
+                alert==true ?
+                <div className="alert alert-warning">
+                    2초이내 구매시 할인
+                </div> : null
+            }
+           
+
+            <button onClick={()=>{setCount(count+1)}}>버튼</button>
             <div className='row'>
             <div className='col-md-6'>
                 <img src={'https://github.com/NujabesCode/NujabesCode.github.io/blob/main/shoes' + (Number(id) + 1) +'.jpg?raw=true'} width='100%'></img>
@@ -18,10 +43,37 @@ function Detail(props){
                 <p>{props.shoes[id].price}</p>
                 <button className='btn btn-danger'>주문하기</button>
             </div>
-            </div>
+        </div>
+
+            <Nav variant="tabs" defaultActiveKey="link-0">
+                <Nav.Item>
+                    <Nav.Link eventKey="link-0" onClick={()=>{탭변경(0)}}>버튼0</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-1" onClick={()=>{탭변경(1)}}>버튼1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-2" onClick={()=>{탭변경(2)}}>버튼2</Nav.Link>
+                </Nav.Item>
+            </Nav>
+            <TabContents 탭={탭}/>
+                        
+
         </div>
 
     )
+}
+
+function TabContents(props){
+
+
+    if(props.탭==0){
+        return <div>내용0</div>
+    }else if(props.탭==1){
+        return <div>내용1</div>
+    }else if(props.탭==2){
+        return <div>내용2</div>
+    }
 }
 
 
